f2be51fa763b3963720361dfdd0be2af1c475d33
Elizaveta Vitalievna <lizagarkina5@gmail.com>
2022-10-30 12:29:46 +0300

    initial

---
+++ app/src/main/java/com/elizav/sportquiz/ui/App.kt
@@ -0,0 +1,8 @@
+package com.elizav.sportquiz.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
---
+++ app/src/main/java/com/elizav/sportquiz/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.elizav.sportquiz.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.elizav.sportquiz.R
+import com.elizav.sportquiz.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.fragmentContainer)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
